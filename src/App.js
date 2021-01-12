4a2a9ebb4c16ca3082b144567862a97afca4169b
Seance <siddharth.dhananjay@gmail.com>
2021-01-12 21:05:51 +1100

    set up router and compiled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
+import Project from "./pages/Discover";
 import About from "./pages/About";
-import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -15,8 +14,7 @@ function App() {
         <Wrapper>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
+          <Route exact path="/project" component={Project} />
         </Wrapper>
         <Footer />
       </div>
