9887e99567d71a6217ccfd9f9edf503b00da107c
Seance <siddharth.dhananjay@gmail.com>
2021-01-12 21:06:37 +1100

    completed the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,19 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-      <span>Pupster 2017</span>
+      <div class="row" id="contact">
+        <div class="medium-12 columns">
+          <h3>Contact Me</h3>
+          <ul class="contact">
+            <li>Email: siddharth.dhananjay@gmail.com</li>
+            <br></br>
+            <li>Phone Number: 0434 067 311</li>
+            <br></br>
+            <li>github: <a href="https://github.com/sid-666">https://github.com/sid-666</a></li>
+            <br></br>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
