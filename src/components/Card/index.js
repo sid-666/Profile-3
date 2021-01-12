e364aa8edd0c8b1b1807f1dd5d72d9e977179bdb
Seance <siddharth.dhananjay@gmail.com>
2021-01-12 21:06:20 +1100

    Finished card for each project

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,26 +1,16 @@
 import React from "react";
-import CardBtn from "../CardBtn";
 import "./style.css";
 
 function Card(props) {
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage: props.image ? `url(${props.image})` : "none"
-      }}
-    >
-      {!props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
-      <CardBtn
-        style={{ opacity: props.image ? 1 : 0 }}
-        onClick={props.handleBtnClick}
-        data-value="pass"
-      />
-      <CardBtn
-        style={{ opacity: props.image ? 1 : 0 }}
-        onClick={props.handleBtnClick}
-        data-value="pick"
-      />
+    <div className="column">
+      <a href={props.deploy} target="_blank">
+        <img className="thumbnail" src={props.image}></img>
+      </a>
+      <a href={props.github} target="_blank">
+        <h5>{props.title}</h5>
+      </a>
+      <p className="details"></p>
     </div>
   );
 }
